46e3a8af7980761666f1005788494f7be6659da2
AnthonyZalev <anthony_zalev@hotmail.com>
2023-04-14 09:53:02 -0400

    cleaned up and checked that pyspark_dataframe works

--- src/pyspark_versions.py
+++ src/pyspark_versions.py
@@ -6,11 +6,9 @@ from  pyspark.sql.functions import *
 
 
 def pyspark_dataframe(file):
-    logFile = file  # Should be some file on your system
+    logFile = file  
     spark = SparkSession.builder.appName("SimpleApp").getOrCreate()
     logData = spark.read.text(logFile).cache()
-    #print(logData.count())
-    #count = logData
 
     # Split by the " " delimiter, get the size of each lines "word" column, and sum them up.
 
