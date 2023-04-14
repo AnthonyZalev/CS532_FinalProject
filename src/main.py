01c9422ae14d72249cc55530b3f59d65cddd7c7f
anthony <anthony_zalev@hotmail.com>
2023-04-14 09:46:07 -0400

    commit

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
 from enlarge_dataset import create_large_dataset
+from pyspark_versions import pyspark_dataframe
 
 def main():
     # Increase locally
-    create_large_dataset(2)
-
+    #create_large_dataset(2)
+    dataset = "src/data/new_shakespeare.txt"
+    pyspark_dataframe(dataset)
 
 if __name__ == "__main__":
     main()
--- src/pyspark_versions.py
+++ src/pyspark_versions.py
@@ -1,7 +1,20 @@
 # Implement in pyspark dataframes, RDD, and sql all the stuff.
 
 
+from pyspark.sql import SparkSession
+from  pyspark.sql.functions import *
+
+
 def pyspark_dataframe(file):
+    logFile = file  # Should be some file on your system
+    spark = SparkSession.builder.appName("SimpleApp").getOrCreate()
+    logData = spark.read.text(logFile).cache()
+    #print(logData.count())
+    #count = logData
+
+    # Split by the " " delimiter, get the size of each lines "word" column, and sum them up.
+
+    logData.select(size(split(logData.value, ' ')).alias('words')).agg(sum('words')).show()
     pass
 
 def pyspark_rdd(file):
