5b8b7920de95e71aa71d77693e21ed952d316bdc
vjawahar <varshaj06@gmail.com>
2023-04-17 18:55:27 -0400

    Fix minor delimiter issue

--- src/native_python.py
+++ src/native_python.py
@@ -1,7 +1,7 @@
 def native_python_count(filename):
-    # Split by the " " delimiter, get the size of each lines "word" column, and sum them up.
+    # Split by the " " delimiter and then calculated length of the resulting array.
     with open(filename) as f:
         lines = f.read()
-    word_count = len(lines.split())
+    word_count = len(lines.split(" "))
     print("Word Count: ", word_count)
     pass
