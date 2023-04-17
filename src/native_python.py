1b991c860cd2cc6252089608ec080129c2adec47
vjawahar <varshaj06@gmail.com>
2023-04-17 19:10:57 -0400

    Use reg ex library for split

--- src/native_python.py
+++ src/native_python.py
@@ -1,7 +1,8 @@
+import re
 def native_python_count(filename):
     # Split by the " " delimiter and then calculated length of the resulting array.
     with open(filename) as f:
         lines = f.read()
-    word_count = len(lines.split(" "))
+    word_count = len(re.split('\s', lines))
     print("Word Count: ", word_count)
     pass
