2df4f15a7277a331eca9c5476d3ec0d5136a0e7c
AnthonyZalev <anthony_zalev@hotmail.com>
2023-05-03 10:40:10 -0400

    now works on s3 bucket

--- src/native_python.py
+++ src/native_python.py
@@ -1,18 +1,10 @@
-import re
+# import 
+from smart_open import smart_open
 def native_python_count(filename):
     # Split by the " " delimiter and then calculated length of the resulting array.
 
-    def read_in_chunks(file_object, chunk_size=1000):
-        """Lazy function (generator) to read a file piece by piece.
-        Default chunk size: 1k."""
-        while True:
-            data = file_object.read(chunk_size)
-            if not data:
-                break
-            yield data
     count = 0
-    with open(filename) as f:
-        for piece in read_in_chunks(f):
-            count += len(piece.split(" "))
-            pass
+    with smart_open(filename, 'rb') as s3_source:
+        for line in s3_source:
+            count += len(line.split(" "))
     print(count)
