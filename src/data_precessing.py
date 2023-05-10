b5c275090524d88004a206b2619322239e3ca318
sabri-xz <sasalabrina@gmail.com>
2023-05-10 15:16:32 -0400

    removed multiple spaces from shakespeare

--- src/data_precessing.py
+++ src/data_precessing.py
@@ -6,6 +6,7 @@ w = open(r"shakespeare_processed.txt", 'w')
 for line in r:
     # removing punctuations and cast all letters to lowercase
     line = line.translate(str.maketrans("", "", string.punctuation)).lower()
+    line = " ".join(line.split())
 
     # only write to output if the line is not empty
     if line != "" and line != '\n':
